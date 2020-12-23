6309132315f4bfd4f0ea4c1a2aa4035ce5d45633
NathaliaMinoque <jr.minoqueen@gmail.com>
2020-12-23 14:20:54 +0700

    buat papan mentah aja

--- SudokuWeekN/SudokuWeekN/Form1.cs
+++ SudokuWeekN/SudokuWeekN/Form1.cs
@@ -19,7 +19,22 @@ namespace SudokuWeekN
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            //Ini codingan bikin textbox array
+            var soal = new TextBox[10, 10];
+            for (var i = 1; i < 10; i++)
+            {
+                for (var j = 1; j < 10; j++)
+                {
+                    var tbox = new TextBox();
+                    tbox.Name = "Soal" + i + j;
+                    tbox.Location = new Point(i * 25, j * 25);
+                    tbox.Visible = true;
+                    tbox.Size = new Size(20, 20);
+                    this.Controls.Add(tbox);
+                    tbox.Text = " ";
+                    soal[i, j] = tbox;
+                }
+            }
         }
     }
 }
